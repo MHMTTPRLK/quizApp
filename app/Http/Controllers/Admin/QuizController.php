897127ebae0774b8e572e45ef2cf0659fcd0141f
MHMTTPRLK <mhmttparlak@gmail.com>
2021-02-22 21:42:49 +0300

    quiz/list.blade Sayfası Oluşturuldu

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -35,7 +35,7 @@ class QuizController extends Controller
      */
     public function store(Request $request)
     {
-       
+
     }
 
     /**
---
+++ resources/views/admin/quiz/list.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+
+        Quizler
+
+    </x-slot>
+    <div class="alert alert-danger">Bootstrap Alert Mesajı</div>
+</x-app-layout>
