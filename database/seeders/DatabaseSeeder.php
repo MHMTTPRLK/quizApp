024f79cbce90688dc7e8d613bc12e0d4437d7520
MHMTTPRLK <mhmttparlak@gmail.com>
2021-02-22 20:41:41 +0300

    Users Tablosu Role Belirlendi Seeder Ve Factory İle Örnek Kullanıcı Bilgileri Ekleme Olayı Düzenlendi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::insert([
+            'name'=>'Mehmet Parlak',
+            'email'=>'mhmttparlak@gmail.com',
+            'email_verified_at' => now(),
+            'type'=>'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+         \App\Models\User::factory(5)->create();
     }
 }
