5ccdb64699256100ccfab6d9546ec6299b058f02
MHMTTPRLK <mhmttparlak@gmail.com>
2021-02-25 20:16:23 +0300

    QueryBuilder , Veri Filtreleme İşlemleri Ayarlandı

--- resources/views/admin/quiz/list.blade.php
+++ resources/views/admin/quiz/list.blade.php
@@ -6,9 +6,31 @@
     </x-slot>
    <div class="card">
        <div class="card-body">
-           <h5 class="card-title">
+           <h5 class="card-title float-right">
                <a href="{{route('quizzes.create')}}" class="btn btn-sm btn-primary"><i class="fa fa-plus"></i> Quiz Oluştur</a>
            </h5>
+           <form method="GET" action="">
+               <div class="form-row">
+                   <div class="col-md-2">
+                       <input type="text" name="title"  value="{{request()->get('title')}}"placeholder="Quiz Adı" class="form-control">
+                   </div>
+                   <div class="col-md-2">
+                       <select name="status" onchange="this.form.submit()" class="form-control">
+                           <option  value="">Durum Seçiniz</option>
+                           <option @if(request()->get('status')=='publish') selected @endif value="publish">Aktif</option>
+                           <option @if(request()->get('status')=='passive') selected @endif value="passive">Pasif</option>
+                           <option @if(request()->get('status')=='draft') selected @endif value="draft">Taslak</option>
+                       </select>
+                   </div>
+                   @if(request()->get('title') || request()->get('status'))
+
+                   <div class="col-md-2">
+                       <a href="{{route('quizzes.index')}}" class="btn btn-secondary btn-sm">Filtre Sıfırlama</a>
+                   </div>
+                  @endif
+               </div>
+           </form>
+           <br>
            <table class="table table-bordered">
                <thead>
                <tr>
@@ -54,7 +76,7 @@
 
                </tbody>
            </table>
-           {{$quizzes->links()}}
+           {{$quizzes->withQueryString()->links()}}
        </div>
    </div>
 </x-app-layout>
